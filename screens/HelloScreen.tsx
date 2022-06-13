06c11c17b5a5d1f685c7f38f8a011ec26aeabc02
m.kumeta <m.kumeta@nakamu-farm.com>
2022-06-13 20:05:43 +0900

    learn useMemo and useCallback

--- screens/HelloScreen.tsx
+++ screens/HelloScreen.tsx
@@ -1,20 +1,30 @@
 import { View, Text } from 'react-native'
-import React, {FC, useEffect} from 'react'
+import React, {FC, useCallback, useEffect, useState} from 'react'
 import tw from 'tailwind-rn';
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParmsList } from '../types/types'
-import { Button } from 'react-native-elements'
+import { Button, Input } from 'react-native-elements'
+import { Child } from '../components/Child';
+import { FontAwesome } from '@expo/vector-icons'
 
 
 type Props = NativeStackScreenProps<RootStackParmsList, 'Hello'>
 
-export const HelloScreen: FC<Props> = ({navigation}) => {
+export const HelloScreen: FC<Props> = ({ navigation }) => {
+    const [text, setText] = useState('')
+    const [printText, setPrintText] = useState('')
+
     useEffect(() => {
         console.log('mounted Hello')
         return () => {
             console.log('un-mounted Hello')
         }
     }, [])
+
+    const printMesg = useCallback(() => {
+        console.log(`Print: ${printText}`)
+    }, [printText])
+    
     return (
         <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
             <Text>HelloScreen</Text>
@@ -24,6 +34,23 @@ export const HelloScreen: FC<Props> = ({navigation}) => {
                     onPress={() => navigation.navigate('ReduxTK')}
                 />
             </View>
+            <Input
+                placeholder='type print text'
+                autoCompleteType={undefined}
+                leftIcon={<FontAwesome name='pencil' size={24} color='gray' />}
+                value={text}
+                onChangeText={(txt) => setText(txt)}
+            />
+            <Text>{text}</Text>
+            <Input
+                placeholder='type print text'
+                autoCompleteType={undefined}
+                leftIcon={<FontAwesome name='pencil' size={24} color='gray' />}
+                value={printText}
+                onChangeText={(txt) => setPrintText(txt)}
+            />
+            <Text>{printText}</Text>
+            <Child printMsg={printMesg} />
         </View>
     )
 }
